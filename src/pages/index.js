622a6a411b4936b029e63a7695c02c50ac791d35
Sara <sarajessvega17@gmail.com>
2021-08-26 09:22:01 -0500

    updated details

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,13 +14,14 @@ export default function IndexPage() {
   return (
     <>
       <Seo title="Gatsby Starter for Portfolio Minimal" />
-      <Page useSplashScreenAnimation>
+      <Page>
+      {/* <Page useSplashScreenAnimation> */}
         <HeroSection sectionId="hero" />
-        <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
-        <AboutSection sectionId="about" heading="About Portfolio Minimal" />
-        <InterestsSection sectionId="details" heading="Details" />
-        <ProjectsSection sectionId="features" heading="Built-in Features" />
-        <ContactSection sectionId="github" heading="Issues?" />
+        {/* <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} /> */}
+        <AboutSection sectionId="about" heading="About Sara" />
+        <InterestsSection sectionId="interests" heading="Interests" />
+        <ProjectsSection sectionId="projects" heading="Latest Projects" />
+        <ContactSection sectionId="github" heading="Contact" />
       </Page>
     </>
   );
